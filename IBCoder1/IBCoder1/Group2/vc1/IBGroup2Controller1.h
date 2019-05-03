7d364fba9e117acd13b14f3251c5d70ebd037ec9
正北飞 <1214569257@qq.com>
2019-05-03 23:38:31 +0800

    总结单例试用场景和原码，反码，补码

---
+++ IBCoder1/IBCoder1/Group2/vc1/IBGroup2Controller1.h
@@ -0,0 +1,17 @@
+//
+//  IBGroup2Controller1.h
+//  IBCoder1
+//
+//  Created by Bowen on 2019/5/3.
+//  Copyright © 2019 BowenCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IBGroup2Controller1 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- IBCoder1/IBCoder1/Main/ViewController2.m
+++ IBCoder1/IBCoder1/Main/ViewController2.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController2.h"
+#import "UIView+Ext.h"
+#import "IBController1.h"
+#import "IBController4.h"
 
-@interface ViewController2 ()
+@interface ViewController2 ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, copy) NSArray *tableArray;
 
 @end
 
@@ -16,17 +22,62 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self initialize];
+    [self setupUI];
 }
 
-/*
-#pragma mark - Navigation
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
+
+- (void)initialize {
+    self.tableArray = @[@"单例，进制转换，原码、反码、补码"];
+}
+
+- (void)setupUI {
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.tableView = ({
+        UITableView *tb = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+        tb.backgroundColor = [UIColor clearColor];
+        tb.dataSource = self;
+        tb.delegate = self;
+        [self.view addSubview:tb];
+        tb;
+    });
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.tableArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellID = @"cellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
+        cell.backgroundColor = [UIColor whiteColor];
+    }
+    cell.textLabel.text = self.tableArray[indexPath.row];
+    cell.textLabel.layer.masksToBounds = YES;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"IBGroup2Controller%ld",indexPath.row+1];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    NSString *controller = [NSString stringWithFormat:@"IBGroup2Controller%ld",indexPath.row+1];
+    Class class = NSClassFromString(controller);
+    
+    UIViewController *vc = [[class alloc] init];
+    vc.title = self.tableArray[indexPath.row];
+    [self.navigationController pushViewController:vc animated:YES];
+
+}
+
+
 
 @end
