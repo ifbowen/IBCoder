b93a8503ddcaeb7c28d85fcbf179abde82959383
正北飞 <1214569257@qq.com>
2020-07-21 23:16:30 +0800

    ”图像渲染“

--- MBCoder/MBCoder/Group1/IBController1/IBController27.m
+++ MBCoder/MBCoder/Group1/IBController1/IBController27.m
@@ -99,133 +99,136 @@
 @end
 
 /*
- iOS提供了两套绘图框架，分别是UIBezierPath和Core Graphics。UIBezierPath属于UIKit。UIBezierPath是对Core Graphics框架的进一步封装。
- OpenGL和Core Graphics都是绘图专用的API类族，调用图形处理器（GPU）进行图形的绘制和渲染。在架构上是平级的，相比UIkit更接近底层。
  
- 一、Quartz
- 在介绍 Core Graphics 之前，我们先把 Quartz 的概念理顺清楚。嫌啰嗦可以直接拉到下面看结论
+ iOS 图像渲染过程解析
  
- 1、简单来说：
- Quartz由Quartz Compositor和Quartz 2D两部分组成
- Core Graphics是基于Quartz框架的2D绘图引擎，同Quartz 2D是等价的。
- CoreAnimation：提供强大的2D和3D动画效果
- CoreImage：给图片提供各种滤镜处理，比如高斯模糊、锐化等
+ 一、图形渲染过程
+ 
+ 1.视图渲染
+ 
+ UIKit是开发中最常用的框架，可以通过设置UIKit组件的布局以及相关属性来绘制界面，显示和动画都通过Core Anmation.
+ 
+ CoreAnimation 是一个复合引擎
+ 提供强大的2D和3D动画效果
+ 它可以快速组合屏幕上不同的可视内容，这些内容被分解成独立的图层(CALayer)，存储在一个图层树的体系中
+ 
+ Core Graphics 是基于Quartz框架的2D绘图引擎，同Quartz 2D是等价的。
+ 主要用于运行时绘制图像。开发者可以使用此框架来处理基于路径的绘图，转换，颜色管理，离屏渲染，图案，渐变和阴影，图像数据管理，
+ 图像创建和图像遮罩以及 PDF 文档创建，显示和分析。
+ 
+ UIBezierPath是对Core Graphics框架的进一步封装。
+ 
+ Core Image 是用来处理运行前创建的图像的。拥有一系列现成的图像过滤器，能对已存在的图像进行高效的处理。
+ 大部分情况下，Core Image 会在 GPU 中完成工作，但如果 GPU 忙，会使用 CPU 进行处理。
+
+ Metal 类似于 OpenGL ES，也是一套第三方标准，具体实现由苹果实现。大多数开发者都没有直接使用过 Metal，但其实所有开发者都在间接地使用 Metal。
+ Core Animation、Core Image、SceneKit、SpriteKit 等等渲染框架都是构建于 Metal 之上的。
+ 
  OpenGL-ES：主要用于游戏绘制，但它是一套编程规范，具体由设备制造商实现
  
+ 2.显示逻辑
+ CoreAnimation提交会话，包括自己和子树（view hierarchy）的layout状态等；
+ RenderServer解析提交的子树状态，生成绘制指令；
+ GPU执行绘制指令；
+ 显示渲染后的数据；
  
- 2、疑问：那么Quartz同OpenGL是什么关系呢？他的底层是否通过OpenGL调用GPU？
- CoreGraphics是基于Quartz框架的绘图引擎，同Quartz 2D是等价的。
- Quartz Extreme是针对Quartz底层的GPU加速。
- Quartz仅使用OpenGL的命令集，直接连接AGP（图形加速接口）
- Quartz在CPU上执行绘图命令，在GPU上最终合成成图形
- QuartzGL是Quartz 2D API的GPU加速。启用QuartzGL后，所有Quartz绘图命令都将转换为OpenGL命令并在GPU上执行。这个模式默认是关闭的
- Quartz在进行3D图形渲染时是基于OpenGL的，通过OpenGL连接AGP
+ 3.提交流程(Commit Transaction)
+ 在 Core Animation 流水线中，app 调用 Render Server 前的最后一步 Commit Transaction 其实可以细分为 4 个步骤：
+ Layout, Display, Prepare, Commit
  
- 二、Core Graphics
-    Core Graphics是基于Quartz框架的高保真输出2D图形的渲染引擎。可处理基于路径的绘图、抗锯齿渲染、渐变、图像、颜色管理、
- PDF文档等。Core Graphics提供了一套2D绘图功能的C语言API，使用C结构体和C的函数模拟了一套面向对象的编程机制。Core Graphics
- 中没有OC的对象和方法。无论图片、PDF还是视图的图层，都是由CoreGraphics框架完成绘制的。UIImage、UIBezierPath和NSString都
- 提供了至少一种用于在drawRect:中绘图的方法，实现原理是将Core Graphics代码封装在其中，降低绘图难度。
+ Layout 阶段主要进行视图构建，包括：LayoutSubviews 方法的重载，addSubview: 方法填充子视图等。
  
- 1、Context
- CoreGraphics中最重要的对象是graphics context，既图形上下文。context是CGContextRef的对象，负责存储绘画状态和绘制内存所处的内存空间。
+ Display 阶段主要进行视图绘制，这里仅仅是设置要成像的图元数据。
+ 重载视图的 drawRect: 方法可以自定义 UIView 的显示，其原理是在 drawRect: 方法内部绘制寄宿图，该过程使用 CPU和内存。
  
- 图形上下文是一个CGContextRef类型的数据，图形上下文相当于画板，用于封装绘图信息(画了什么)和绘图状态(线条大小、颜色等)，它决定绘制的输出目标
- (绘制到什么地方去)，目标可以是PDF文件、bitmap或者显示器的窗口。
- 
- 相同的一套绘图序列，指定不同的图形上下文，就可将相同的图像绘制到不同的目标上，目标可以是PDF文件、bitmap或者显示器的窗口。
- 
- QuartZ 2D提供了 5 种类型的图形上下文：Bitmap Graphics Context、PDF Graphics Context、Window Graphics Context、
- Layer Context、Post Graphics Context。
- 
- 在UIView中，系统会默认创建一个Layer Graphics Context，它对应UIView的layer属性，该图形上下文可以在drawRect:方法中获取，
- 开发者只能获取，不能自己重新创建，在该图层上下文中绘制的图形，最终会通过CALayer显示出来。因此，View之所以能显示东西，完全是因为它内部的layer。
- 
- 2、图形上下文的图形状态堆栈
-    图形上下文中包含一个保存过的图形状态堆栈。在QuartZ 2D创建图形上下文时，该堆栈是空的。
-    CGContextSaveGState()函数的作用是将当前图形状态推入堆栈。之后，您对图形状态所做的修改会影响随后的描画操作，
- 但不影响存储在堆栈中的拷贝。
-    CGContextRestoreGState()函数，修改完成后使用此函数把堆栈顶部的状态弹出，返回到之前的图形状态。这种推入和
- 弹出的方式是回到之前图形状态的快速方法，避免逐个撤消所有的状态修改；这也是将某些状态（比如裁剪路径）恢复到原有设置的唯一方式。
+ Prepare 阶段属于附加步骤，一般处理图像的解码和转换等操作。
 
-3、Core Graphics的Ref后缀类型
-   带有Ref后缀的类型是CoreGraphics中用来模拟面向对象机制的C结构。CoreGraphics对象是在堆上分配内存，因此创建CoreGraphics对象时，
- 会返回一个指向对象内存地址的指针。
-   使用这种分配方式的C结构都有一个用来表示结构指针的类型定义（type definition）。例如，CGColor结构，不会被直接使用的类型，有一个表示
- Color * 的类型定义—CGColorRef，用来被使用的类型。使用这种类型定义是为了区分指针变量，方便开发者判断指针变量是指向C结构还是可以接收消
- 息的Objective-C对象。
-   CGRect和CGPoint这种比较简单直接在栈上分配的结构体，不需要使用结构指针，因此类型名称后不带Ref后缀。
-   带有Ref后缀的类型的对象可能是强引用指针，成为指向对象的拥有者。ARC是无法识别Core对象的所有权，必须在使用后手动释放。规则是，如果使用名
- 称中带有create或者copy的函数创建了一个CoreGraphics对象，就必须调用Release函数并传入该对象的指针。
+ Commit 阶段主要将图层进行打包(序列化)，并将它们发送至 Render Server。该过程会递归执行，因为图层和视图都是以树形结构存在。
  
- 3、作用
- 绘制图形 : 线条、三角形、矩形、圆、圆弧弧等
- 绘制文字
- 绘制生成图片(图像)
- 读取生成PDF
- 截图
- 绘制渐变
- 自定义UI控件
- 
- 4、总结
-    UIBezierPath的优势是方便，能用最少的代码得要想要的图形，并且不需要管理图形上下文、缓冲区等容易出问题的地方，只需要关注图形本身就行了。
- 最主要的缺点是支持的效果有限，当需要实现一些复杂图形、复杂渐变效果的时候就无能为力了。所以如果只是一个简单的图形没有特别的要求，
- 可以用UIBezierPath实现。
-    Core Graphics的功能就比UIBezierPath强大很多，使用起来也更复杂，而且需要自己管理图形上下文，需要投入更多的开发工作量。
- 在效率和可做更多工作这两个方面上Core Graphics全面压制UIBezierPath，所以如果是复杂的图形、多个图形叠加、多种颜色渐变等
- 需求可以使用Core Graphics实现。
- 
- 
- 三、UIView和CALayer的区别
- 对于继承
- UIView —> UIResponder —> NSObject
- CALayer —> NSObject
- 
- 对于响应用户事件
- UIView继承自UIResponder，UIResponder是用来响应事件的，所以UIView可以响应事件
- CALayer直接继承于NSObject，所以CALayer不能响应事件
- 
- 对于所属框架
- UIView是在/System/Library/Frameworks/UIKit.framework中定义，UIKit主要是用来构建用户界面，并且是可以响应事件的
- CALayer是在/System/Library/Frameworks/QuartzCore.framework定义，2D图像绘制都是通过QuartzCore.framework实现的
- 
- 对于基本属性
- UIView：position、size、transform
- CALayer：position、size、transform、animations
- 
- 四、UIView的显示原理
-    因为UIView依赖于CALayer提供的内容，而CALayer又依赖于UIView提供的容器来显示绘制的内容，所以UIView的显示可以说
- 是CALayer要显示绘制的图形。当要显示时，CALayer会准备好一个CGContextRef(图形上下文)，然后调用它的delegate(这里就
- 是UIView)的drawLayer:inContext:方法，并且传入已经准备好的CGContextRef对象，在drawLayer:inContext:方法中
- UIView又会调用自己的drawRect:方法。
-    我们可以把UIView的显示比作“在电脑上使用播放器播放U盘上得电影”，播放器相当于UIView，视频解码器相当于CALayer，U盘
- 相当于CGContextRef，电影相当于绘制的图形信息。不同的图形上下文可以想象成不同接口的U盘。
- 
- 注意：当我们需要绘图到根层上时，一般在drawRect:方法中绘制，不建议在drawLayer:inContext:方法中绘图
- 
- 总结
-UIView相比CALayer最大区别是UIView可以响应用户事件，而CALayer不可以。
- UIView侧重于对显示内容的管理，CALayer侧重于对内容的绘制。
- 对于UIView所管理的内容，显示任何图形都会受到CALayer的影响。UIView依赖于CALayer提供的内容，
- CALayer依赖于UIView提供的容器来显示绘制的内容。UIView与CALayer都有树状层级结构，
- CALayer内部有SubLayers，UIView内部也有SubViews。
+ 4.动画渲染原理
+ 一般使用 UIView Animation 实现，iOS 将其处理过程分为如下三部阶段：
+ Step 1：调用 animationWithDuration:animations: 方法
+ Step 2：在 Animation Block 中进行 Layout，Display，Prepare，Commit 等步骤。
+ Step 3：Render Server 根据 Animation 逐帧进行渲染。
 
+ 二、UIView与CALayer的关系
+ UIKit 中的每一个 UI 视图控件其实内部都有一个关联的 CALayer
+ 由于这种一一对应的关系，视图层级拥有 视图树 的树形结构，对应 CALayer 层级也拥有 图层树 的树形结构。
+ 其中，视图的职责是 创建并管理 图层，以确保当子视图在层级关系中 添加或被移除 时，其关联的图层在图层树中也有相同的操作，即保证视图树和图层树在结构上的一致性。
  
- 五、iOS系统使用OpenGL编程流程
+ 1、那么为什么 iOS 要基于 UIView 和 CALayer 提供两个平行的层级关系呢？
+ 其原因在于要做 职责分离，这样也能避免很多重复代码。在 iOS 和 Mac OS X 两个平台上，事件和用户交互有很多地方的不同，
+ 基于多点触控的用户界面和基于鼠标键盘的交互有着本质的区别，
+ 这就是为什么 iOS 有 UIKit 和 UIView，
+ 对应 Mac OS X 有 AppKit 和 NSView 的原因。
+ 它们在功能上很相似，但是在实现上有着显著的区别。
+
+ 实际上，这里并不是两个层级关系，而是四个。每一个都扮演着不同的角色。除了 视图树 和 图层树，还有 呈现树 和 渲染树。
+
+ 2、CALayer
+ 那么为什么 CALayer 可以呈现可视化内容呢？因为 CALayer 基本等同于一个 纹理。纹理是 GPU 进行图像渲染的重要依据。
+ CALayer 包含一个 contents 属性指向一块缓存区，称为 backing store，可以存放位图（Bitmap）。iOS 中将该缓存区保存的图片称为 寄宿图。
+ 在实际开发中，绘制界面也有两种方式：一种是 手动绘制；另一种是 使用图片。
+
+ 1、Contents Image 是指通过 CALayer 的 contents 属性来配置图片。然而，contents 属性的类型为 id。
+ 在这种情况下，可以给 contents 属性赋予任何值，app 仍可以编译通过。但是在实践中，如果 content 的值不是 CGImage ，得到的图层将是空白的。
+ 为什么要将 contents 的属性类型定义为 id 而非 CGImage。这是因为在 Mac OS 系统中，该属性对 CGImage 和 NSImage 类型的值都起作用，
+ 而在 iOS 系统中，该属性只对 CGImage 起作用。
+ 本质上，contents 属性指向的一块缓存区域，称为 backing store，可以存放 bitmap 数据。
  
- 1、设置CAEAGLLayer
- CAEAGLLayer是iOS中用于呈现OpenGL ES的渲染内容的
+ 2、Custom Drawing 是指使用 Core Graphics 直接绘制寄宿图。实际开发中，一般通过继承 UIView 并实现 -drawRect: 方法来自定义绘制。
+ 底层是 CALayer 完成了重绘工作并保存了产生的图片。
  
- 2、设置EAGLContext
- OpenGL ES 渲染上下文。这个context管理所有使用OpenGL ES 进行描绘的状态，命令以及资源信息。
+ 1）CALayer 有一个可选的 delegate 属性，实现了 CALayerDelegate 协议。UIView 作为 CALayer 的代理实现了 CALayerDelegae 协议。
+ 2）当需要重绘时，即调用 -drawRect:，CALayer 请求其代理给予一个寄宿图来显示。
+ 3）CALayer 首先会尝试调用 -displayLayer: 方法，此时代理可以直接设置 contents 属性。
+ 4）如果代理没有实现 -displayLayer: 方法，CALayer 则会尝试调用 -drawLayer:inContext: 方法。
+ 在调用该方法前，CALayer 会创建一个空的寄宿图（尺寸由 bounds 和 contentScale 决定）和一个 Core Graphics 的绘制上下文，
+ 为绘制寄宿图做准备，作为 ctx 参数传入。
+ 5）最后，由 Core Graphics 绘制生成的寄宿图会存入 backing store。
+
+
+ 三.渲染服务(Render Server)
+ 1）渲染服务进程首先将打包上来的图层进行解压(反序列化)，得到图层树。
+
+ 2）然后依据图层树中图层的顺序、RGBA值、图层的frame等，对被遮挡的图层进行过滤。
+
+ 3）Core Animation进行过滤以后将图层树转化为渲染树。
+ 渲染树就是指图层树对应每个图层的信息，比如顶点坐标、顶点颜色这些信息，抽离出来，形成的树结构，就叫渲染树了
+ 然后将渲染树信息递归提交给OpenGL ES /Metal。
+
+ 4）OpenGL ES /Metal会编译、链接可编程的顶点着色器和片元着色器程序(如果有对顶点着色器和片元着色器进行自定义)，
+ 并结合固定的渲染管线，生成绘制命令，并提交到命令缓冲区CommandBuffer `，供GPU读取调用。
+
+ 四. 图形渲染管线(Graphics Rendering Pipeline)
  
- 3、创建Renderbuffer
- 缓冲区用于存储绘图数据，Render Buffer有三种类型，分别是color、depth、stencil buffer
+ Graphics Rendering Pipeline，图形渲染管线，实际上指的是一堆原始图形数据途经一个输送管道，期间经过各种变化处理最终出现在屏幕的过程。
+ 通常情况下，渲染管线可以描述成 vertices(顶点) 到 pixels(像素) 的过程。
  
- 4、创建Framebuffer object
- Framebuffer object是buffer的管理者，color、depth、stencil可以添加到一个Framebuffer object上
+ 图形渲染管线的主要工作可以被划分为两个部分：
+ 把 3D 坐标转换为2D 坐标
+ 把 2D 坐标转变为实际的有颜色的像素
  
- 5、销毁Renderbuffer和Framebuffer
- 当UIView变化后，layer的宽高也随之变化，导致原来的renderbuffer 不再相符，需要销毁既有 renderbuffer 和 framebuffer
+ GPU 图形渲染管线的具体实现可分为六个阶段，如下图所示。
+ 顶点着色器（Vertex Shader）
+ 形状装配（Shape Assembly），又称 图元装配
+ 几何着色器（Geometry Shader）
+ 光栅化（Rasterization）
+ 片段着色器（Fragment Shader）
+ 测试与混合（Tests and Blending）
+ 
+ 五.屏幕显示
+ iOS 设备会始终使用双缓存，并开启垂直同步。
+ 由于垂直同步的机制，如果在一个 VSync 时间内，CPU或者 GPU 没有完成内容提交，则那一帧就会被丢弃，等待下一次机会再显示，
+ 而这时显示屏会保留之前的内容不变。这就是界面卡顿的原因。
+ 双缓冲工作原理:GPU 会预先渲染一帧放入一个缓冲区中，用于视频控制器的读取。当下一帧渲染完毕后，GPU 会直接把视频控制器的指针指向第二个缓冲器。
+ 
+ 屏幕图像显示的原理，需要先从 CRT 显示器原理说起
+ CRT 的电子枪从上到下逐行扫描，扫描完成后显示器就呈现一帧画面。
+ 然后电子枪回到初始位置进行下一次扫描。为了同步显示器的显示过程，显示器会用硬件时钟产生一系列的定时信号。
+ 当电子枪换行进行扫描时，显示器会发出一个水平同步信号（horizonal synchronization），简称 HSync；
+ 而当一帧画面绘制完成后，电子枪回复到原位，
+ 准备画下一帧前，显示器会发出一个垂直同步信号（vertical synchronization），简称 VSync。
+ 显示器通常以固定频率进行刷新，这个刷新率就是VSync 信号产生的频率。虽然现在的显示器基本都是液晶显示屏了，但其原理基本一致。
  
  */
